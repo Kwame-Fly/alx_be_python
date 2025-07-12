c31c625d5f292ac444fc0e777f151cd16e71738a
Kwame-Fly <kwametkodua@gmail.com>
2025-07-12 20:33:08 +0000

    newly created file

---
+++ basic_operations.py
@@ -0,0 +1,11 @@
+#Perform addition, subtraction, and multiplication on these numbers
+number1 = 10
+number2 = 5
+#Calculation of operations
+sum_result = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+#Output of results
+print(f"Addition of {number1} and {number2} is {sum_result}")
+print(f"Subtraction of {number1} and {number2} is {difference}")
+print(f"Multiplication of {number1} and {number2} is {product}")
