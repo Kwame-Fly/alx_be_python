fcf47317e74a910b8f80d1b01ec495918dd4dd1e
Kwame-Fly <kwametkodua@gmail.com>
2025-07-12 21:19:07 +0000

    new simple_interest file

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -2,6 +2,6 @@
 principal = 1000
 rate = 0.05
 time = 3
-simple_interest = float(principal * rate * time) 
+simple_interest = float(principal*rate*time) 
 #output of simple interest
 print (f"The simple interest is: {simple_interest}")
