3282f152aaca9ec1ccbdc30fd9487c88dc3b2e1a
Kwame-Fly <kwametkodua@gmail.com>
2025-07-12 21:13:40 +0000

    "simple interest file"

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,7 @@
+#simple interest earned on an investment over a period of time
+principal = 1000
+rate = 0.05
+time = 3
+simple_interest = float(principal * rate * time) 
+#output of simple interest
+print (f"The simple interest is: {simple_interest}")
