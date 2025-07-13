ac9938c1f0a7eaaddde3615f37855816aa518c29
Kwame-Fly <kwametkodua@gmail.com>
2025-07-13 06:04:54 +0000

    created hours to seconds file

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,7 @@
+#Converting hours to seconds
+hours = 2
+seconds = 3600
+# calculating hours into seconds
+result = hours * seconds
+#result of hours into seconds
+print (f"{hours} hour(s) is {result} seconds")
